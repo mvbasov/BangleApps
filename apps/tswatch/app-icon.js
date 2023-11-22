bcd9161df19473202077fb6d8ef32528b72e0394
Mikhail Basov (XMi8) <mikhail[at]basov[dot]net>
2023-11-22 22:41:34 +0300

    Another one app icon

--- apps/tswatch/app-icon.js
+++ apps/tswatch/app-icon.js
@@ -1,1 +1,1 @@
-require("heatshrink").decompress(atob("mEwgIbYh////wAoMQAQMIAQMgAQMEAQNAAQMCAQOAAQI0BGwmAAskOjkcnAFBiMikVAAocCAomCBZUMDokCggXEAogLFkVEHY5ljgO2mwLIigEB1AjSXIrFGaIrdFdIoAWA="))
+require("heatshrink").decompress(atob("mEwgMAh////wBIMIAQMQAQMEAQMgAo4XFj4FB/AUHoACBgQCBwACBGgIBBAAWAgYFD4AFYj08AIIFHmdmmdgAocDAomDAokPvnAj4FBhgdBiYXCgwFDwYFEBYoBBDoJBLMq8B202ApEUCAOoAo4jKVoq5FYozRFborpFBwUQAo8IAQgXCDoI"))
